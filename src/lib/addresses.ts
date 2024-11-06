3ea5c5685ff851c3f3db1e18e2dced38bb79f184
0xyshv <writetoyshv@gmail.com>
2024-11-06 17:11:10 +0530

    chore: add usdt addresses

--- src/lib/addresses.ts
+++ src/lib/addresses.ts
@@ -6,8 +6,8 @@ export const usdcMultiChainAddresses = {
 };
 
 export const usdtMultiChainAddresses = {
-  polygon: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
-  base: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
-  optimism: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
-  arbitrum: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+  polygon: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+  base: "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2",
+  optimism: "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58",
+  arbitrum: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
 };
