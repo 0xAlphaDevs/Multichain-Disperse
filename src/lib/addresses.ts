94fddc813c7f1c9d96c42384636f71e1f6855265
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-11-06 17:03:47 +0530

    chore: add multichain usdc addresses

--- src/lib/addresses.ts
+++ src/lib/addresses.ts
@@ -1,7 +1,13 @@
-export const polygonUsdcAddress = "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359";
+export const usdcMultiChainAddresses = {
+  polygon: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+  base: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  optimism: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
+  arbitrum: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+};
 
-export const baseUsdcAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
-
-export const optimismUsdcAddress = "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85";
-
-export const arbitrumUsdcAddress = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
+export const usdtMultiChainAddresses = {
+  polygon: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+  base: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  optimism: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
+  arbitrum: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+};
