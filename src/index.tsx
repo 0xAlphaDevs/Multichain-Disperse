ef92c52776d57cd7061083feab9fa2637853d5ce
0xyshv <writetoyshv@gmail.com>
2024-11-06 09:42:05 +0530

    chore: sync

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,28 @@
 'use client';
 
-import demoImage from '@/assets/demo.gif';
-import Header from '@/components/header';
-import { useAccount } from '@particle-network/connectkit';
+import { ConnectButton, useAccount } from '@particle-network/connectkit';
 import { isEVMChain } from '@particle-network/connectkit/chains';
-import Image from 'next/image';
 import Demo from './components/demo';
-import styles from './index.module.css';
 
 export default function Index() {
   const { isConnected, chain } = useAccount();
 
   return (
-    <>
-      <Header />
-      <main className={styles['main-content']}>
+    <div className="flex flex-col min-h-screen">
+      <header className='flex items-center justify-between py-4 px-8 bg-purple-900 text-white'>
+        <p className='text-lg font-bold'>Multichain Disperse</p>
+        <ConnectButton />
+      </header>
+      <main className="flex-grow flex justify-center items-center p-8">
         {isConnected && chain && isEVMChain(chain) ? (
           <Demo />
         ) : (
-          <Image sizes='100%' src={demoImage} style={{ width: '100%' }} alt='demo' />
+          <div className='flex flex-col items-center gap-4 justify-center text-center'>
+            <p className='text-lg'>Please connect your wallet to get started with</p>
+            <p className='font-semibold text-5xl text-purple-600'>Multichain Disperse</p>
+          </div>
         )}
       </main>
-    </>
+    </div>
   );
 }
