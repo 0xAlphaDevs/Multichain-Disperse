8a7fb5e815e9aaac08733e4a4bc2a254c81e64e3
0xyshv <writetoyshv@gmail.com>
2024-11-08 15:58:02 +0530

    chore: add states to the DisperseTabs

--- src/components/disperse/DisperseTabs.tsx
+++ src/components/disperse/DisperseTabs.tsx
@@ -12,7 +12,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
 import { Label } from "@/components/ui/label";
 import { PlusCircle, X } from "lucide-react";
 import { Card } from "@/components/ui/card";
@@ -28,6 +27,8 @@ export function DisperseTabs() {
   const [tokenRows, setTokenRows] = useState([
     { address: "", chain: "", amount: "" },
   ]);
+  const [etherTextareaContent, setEtherTextareaContent] = useState("");
+  const [tokenTextareaContent, setTokenTextareaContent] = useState("");
 
   const addRow = (type: "ether" | "token") => {
     if (type === "ether") {
@@ -68,7 +69,40 @@ export function DisperseTabs() {
     }
   };
 
+  const parseTextareaContent = (content: string) => {
+    return content
+      .split('\n')
+      .map(line => {
+        const [address, chain, amount] = line.split(',').map(item => item.trim());
+        return { address, chain, amount };
+      })
+      .filter(row => row.address && row.chain && row.amount);
+  };
+
   const handleSendTransaction = () => {
+    let dataToSend = [];
+
+    if (transferType === "ether") {
+      if (etherTextareaContent.trim()) {
+        dataToSend = parseTextareaContent(etherTextareaContent);
+      } else {
+        dataToSend = etherRows.filter(row => row.address && row.chain && row.amount);
+      }
+      console.log("Ether Transaction Data:", dataToSend);
+    } else {
+      if (tokenTextareaContent.trim()) {
+        dataToSend = parseTextareaContent(tokenTextareaContent);
+      } else {
+        dataToSend = tokenRows.filter(row => row.address && row.chain && row.amount);
+      }
+      console.log("Token Transaction Data:", dataToSend);
+    }
+
+    if (dataToSend.length === 0) {
+      toast.error("Please fill in all required fields");
+      return;
+    }
+
     toast.success("Transaction sent successfully!");
   };
 
@@ -103,12 +137,14 @@ export function DisperseTabs() {
                   onChange={(e) =>
                     updateRow("ether", index, "address", e.target.value)
                   }
+                  required
                 />
                 <Select
                   value={row.chain}
                   onValueChange={(value) =>
                     updateRow("ether", index, "chain", value)
                   }
+                  required
                 >
                   <SelectTrigger className="w-[200px]">
                     <SelectValue placeholder="Select Chain" />
@@ -128,8 +164,9 @@ export function DisperseTabs() {
                   onChange={(e) =>
                     updateRow("ether", index, "amount", e.target.value)
                   }
+                  required
                 />
-                {index >= 0 && (
+                {index > 0 && (
                   <Button
                     variant="ghost"
                     size="icon"
@@ -162,6 +199,8 @@ export function DisperseTabs() {
             <Textarea
               placeholder="Paste comma separated values here&#10;Format: address,chain,amount&#10;Example: 0x123...,polygon,0.1"
               className="min-h-[100px]"
+              value={etherTextareaContent}
+              onChange={(e) => setEtherTextareaContent(e.target.value)}
             />
             <div className="flex justify-center pt-6 pb-4">
               <Button onClick={handleSendTransaction}>Send Transaction</Button>
@@ -171,7 +210,7 @@ export function DisperseTabs() {
 
         <TabsContent value="token" className="space-y-4 px-4 pb-4">
           <div className="space-y-4 mt-4">
-            <Select onValueChange={setSelectedToken}>
+            <Select onValueChange={setSelectedToken} required>
               <SelectTrigger>
                 <SelectValue placeholder="Select Token" />
               </SelectTrigger>
@@ -199,7 +238,7 @@ export function DisperseTabs() {
               </SelectContent>
             </Select>
 
-            {selectedToken == "usdc" && (
+            {selectedToken && (
               <>
                 <div className="flex justify-end">
                   <AllocationComponent token={selectedToken} />
@@ -217,12 +256,14 @@ export function DisperseTabs() {
                       onChange={(e) =>
                         updateRow("token", index, "address", e.target.value)
                       }
+                      required
                     />
                     <Select
                       value={row.chain}
                       onValueChange={(value) =>
                         updateRow("token", index, "chain", value)
                       }
+                      required
                     >
                       <SelectTrigger className="w-[200px]">
                         <SelectValue placeholder="Select Chain" />
@@ -242,8 +283,9 @@ export function DisperseTabs() {
                       onChange={(e) =>
                         updateRow("token", index, "amount", e.target.value)
                       }
+                      required
                     />
-                    {index >= 0 && (
+                    {index > 0 && (
                       <Button
                         variant="ghost"
                         size="icon"
@@ -278,92 +320,8 @@ export function DisperseTabs() {
                 <Textarea
                   placeholder="Paste comma separated values here&#10;Format: address,chain,amount&#10;Example: 0x123...,polygon,100"
                   className="min-h-[100px]"
-                />
-                <div className="flex justify-center pt-6 pb-4">
-                  <Button onClick={handleSendTransaction}>
-                    Send Transaction
-                  </Button>
-                </div>
-              </>
-            )}
-
-            {selectedToken == "usdt" && (
-              <>
-                <AllocationComponent token={selectedToken} />
-
-                {tokenRows.map((row, index) => (
-                  <div
-                    key={index}
-                    className="flex items-center gap-4 justify-start"
-                  >
-                    <Input
-                      placeholder="Address"
-                      className="w-[500px]"
-                      value={row.address}
-                      onChange={(e) =>
-                        updateRow("token", index, "address", e.target.value)
-                      }
-                    />
-                    <Select
-                      value={row.chain}
-                      onValueChange={(value) =>
-                        updateRow("token", index, "chain", value)
-                      }
-                    >
-                      <SelectTrigger className="w-[200px]">
-                        <SelectValue placeholder="Select Chain" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="polygon">Polygon</SelectItem>
-                        <SelectItem value="arbitrum">Arbitrum</SelectItem>
-                        <SelectItem value="optimism">Optimism</SelectItem>
-                        <SelectItem value="base">Base</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <Input
-                      placeholder="Amount"
-                      type="number"
-                      className="w-[120px]"
-                      value={row.amount}
-                      onChange={(e) =>
-                        updateRow("token", index, "amount", e.target.value)
-                      }
-                    />
-                    {index >= 0 && (
-                      <Button
-                        variant="ghost"
-                        size="icon"
-                        onClick={() => removeRow("token", index)}
-                        className="text-red-500 hover:text-red-700"
-                      >
-                        <X className="h-4 w-4" />
-                      </Button>
-                    )}
-                  </div>
-                ))}
-                <Button
-                  variant="outline"
-                  className="w-full mt-2"
-                  onClick={() => addRow("token")}
-                >
-                  <PlusCircle className="mr-2 h-4 w-4" />
-                  Add Row
-                </Button>
-
-                <div className="relative">
-                  <div className="absolute inset-0 flex items-center">
-                    <span className="w-full border-t border-purple-300" />
-                  </div>
-                  <div className="relative flex justify-center text-xs uppercase">
-                    <span className="bg-background px-2 text-purple-500">
-                      or
-                    </span>
-                  </div>
-                </div>
-
-                <Textarea
-                  placeholder="Paste comma separated values here&#10;Format: address,chain,amount&#10;Example: 0x123...,polygon,100"
-                  className="min-h-[100px]"
+                  value={tokenTextareaContent}
+                  onChange={(e) => setTokenTextareaContent(e.target.value)}
                 />
                 <div className="flex justify-center pt-6 pb-4">
                   <Button onClick={handleSendTransaction}>
