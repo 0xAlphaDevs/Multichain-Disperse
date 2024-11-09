21c4bea8a7964083b47e577cd79c55377e5d1cc9
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-11-09 13:32:56 +0530

    chore: add function chainId to chain name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function chainIdToChainName(chainId: string) {
+  switch (chainId) {
+    case "1":
+      return "Ethereum";
+    case "10":
+      return "Optimism";
+    case "8453":
+      return "Base";
+    case "42161":
+      return "Arbitrum";
+    default:
+      return "Unknown";
+  }
 }
